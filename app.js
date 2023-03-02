3287749636a05e0530534daefd945d679707acf7
Shivam <sy654906@gmail.com>
2023-03-03 01:12:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// let userInput = prompt("Enter password");
+// if (userInput.length <= 8 && userInput.indexOf(' ') === -1) {
+//     console.log("Valid Password!!");
+// } else {
+//     console.log("Invalid Password!!");
+// }
+
+
+
+// let userInput=prompt("Enter password:");
+// if(userInput.length<=8){
+//     if(userInput.indexOf(' ')){
+//         console.log("Valid Password!!");
+//     }else{
+//         console.log("NO space is allowed.");
+//     }
+// }else{
+//     console.log("Password length must be lesser or equal to 8.");
+// }
+
+
+
+// if (userInput.length > 8) {
+//     if (userInput.indexOf(' ') !== -1) {
+//         console.log("NO space is allowed & Password length must be lesser or equal to 8.");
+//     } else {
+//         console.log("Password length must be lesser or equal to 8.")
+//     }
+// } else if (userInput.length <= 8 && userInput.indexOf(' ') !== -1) {
+//     console.log("NO space is allowed");
+// } else {
+//     console.log("Valid Password!!");
+// }
+
+// let userInput = prompt("Enter password:");
+// if(userInput)
+// {
+//     console.log("Truthy!");
+// }
+// else{
+//     console.log("FAlsy!");
+// }
+
+// let input=prompt("Hey,say something!")
+// while(true)
+// {
+//     input=prompt(input);
+//     if(input.toLowerCase()==="stop copying me")
+//     {
+//         break;
+//     }
+// }
+
